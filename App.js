b09e93b7f0d7f7c9d5ae0508b96247dabdc451a9
namchin <namchin0529@gmail.com>
2022-07-25 23:12:01 +0800

    :fire: Screen stracture created

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
+
 import {
   SafeAreaView,
   ScrollView,
@@ -17,76 +9,38 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-// Start here and here and here(3)
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import QuestionScreen from './Screens/QuestionScreen';
+import MuushigScreen from './Screens/MuushigScreen';
+import ResultScreen from './Screens/ResultScreen';
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const Stack = createNativeStackNavigator();
+
+function App() {
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='QuestionScreen'
+        screenOptions={{ title: null, headerStyle: { elevation: 0 } }}
+      >
+        <Stack.Screen
+          name='QuestionScreen'
+          component={QuestionScreen}
+          options={{ headerShown:false }} />
+          <Stack.Screen
+          name='MuushigScreen'
+          component={MuushigScreen}
+          options={{ headerShown:false }} />
+          <Stack.Screen
+          name='ResultScreen'
+          component={ResultScreen}
+          options={{ headerShown:false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
@@ -94,18 +48,6 @@ const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
   },
 });
 
---
+++ Screens/QuestionScreen.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    Button
+} from 'react-native';
+
+function App({ navigation }) {
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Question Screen !!!</Text>
+            <Button title='Go next' onPress={() => navigation.navigate('MuushigScreen')} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: '600',
+    },
+    sectionDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: '400',
+    },
+    highlight: {
+        fontWeight: '700',
+    },
+});
+
+export default App;
